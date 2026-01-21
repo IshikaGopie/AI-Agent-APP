49d5a972d2c8c16f8895179dea6ae534ca9d3cf3
IshikaGopie <ishikagopie123@gmail.com>
2026-01-20 23:46:06 -0400

    feature #1: updated chat to render markdown

--- src/components/atoms/MessageBubble.jsx
+++ src/components/atoms/MessageBubble.jsx
@@ -1,4 +1,8 @@
 import { Box } from "@mui/material";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark, oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const MessageBubble = ({ children, isAgent }) => {
     return (
@@ -12,7 +16,30 @@ const MessageBubble = ({ children, isAgent }) => {
                 border: isAgent ? "1px solid #e5e7eb" : "none",
             }}
         >
-            {children}
+            <ReactMarkdown
+                remarkPlugins={[remarkGfm]}
+                components={{
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || "");
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                style={isAgent ? oneLight : oneDark}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}
+                            >
+                                {String(children).replace(/\n$/, "")}
+                            </SyntaxHighlighter>
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        );
+                    },
+                }}
+            >
+                {children}
+            </ReactMarkdown>
         </Box>
     );
 };
